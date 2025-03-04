84817276be750c9570bd13d6cde73b5ae909f827
Arjun <arjun.s.jadhav1@gmail.com>
2025-03-04 20:56:07 +0800

    First_Publish

---
+++ Lesson; 6/L;6_project1.py
@@ -0,0 +1,15 @@
+v1 = input("DO *NOT* ENTER A CHARACTER WHICH IS NOT AN INTEGER, FLOAT OR STRING. please Enter a integer, float or string here:  ")
+try:
+    # input is integer
+    val = int(v1)
+    print("this is not a string, this is a integer")
+except ValueError:
+    try:
+        # input is float
+        val = float(v1)
+        print("This is not a string, this is a float.")
+              
+           #input is .str 
+    except ValueError:
+        val = str(v1)
+        print("This is infact a string!")
