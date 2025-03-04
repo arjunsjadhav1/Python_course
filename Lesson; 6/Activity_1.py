0999923a802c683084b4307fb85dfff7d9f68cd1
Arjun <arjun.s.jadhav1@gmail.com>
2025-03-04 20:17:00 +0800

    first_publish

---
+++ Lesson; 6/Activity_1.py
@@ -0,0 +1,19 @@
+v1=10
+v2=12
+v3=0
+if v1 and v2 and v3:
+    print("all of the numbers have the bolean value of 'true'")
+else:
+    print("at least one of the numbers have a bolean value of false")
+v1=10
+v2=-10
+v3=0
+if v1>0 or v2>0:
+    print("at least one of the numbers are greater than 0")
+else:print("both numbers are lesser than that of 0")
+if v2>0 or v3>0:
+    print("at least one of the numbers are greater than 0")
+else:
+    print("both numbers are lesser than that of 0")
+
+
---
+++ Lesson; 6/Activity_2.py
@@ -0,0 +1,20 @@
+a=10
+b=12
+c=12
+print(a!=b)
+print(b!=c)
+a="python"
+b="coding"
+if a!=b:
+    print(a,"and",b,"are not equal")
+d=4
+e=5
+if (d==1)!=(e==5):
+    print("true")
+else:
+    print("false")
+f=int(input("enter a number"))
+if f%2!=0:
+    print(f,"is an odd number.")
+else:
+    print(f,"is an even number.")
