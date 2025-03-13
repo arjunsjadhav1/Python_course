9386cca3f376c1afb1ba91983d065e525e42fc9c
Arjun <arjun.s.jadhav1@gmail.com>
2025-03-13 20:29:43 +0800

    FIrst_Publish

---
+++ Lesson; 10/Activity_1.py
@@ -0,0 +1,10 @@
+# calculate sum of whole
+V1=int(input("enter number"))
+for I in range(1,V1+1):    
+    print("sum is equal to",I*2)
+
+
+    
+
+
+
---
+++ Lesson; 10/Activty_2.py
@@ -0,0 +1,49 @@
+s1=input("Input a string here:  ")
+s2=""
+for I in s1:
+    s2=I+s2
+print(s2)
+
+_=f+_
+ =f
+
+fo=o+fo
+ =ffo
+
+ffo=r+ffo
+  =fffo
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
