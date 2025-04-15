fe28a5717b5169222e12b78cac1f34ede33e2b53
Arjun <arjun.s.jadhav1@gmail.com>
2025-04-15 20:46:46 +0800

    First_publish

---
+++ lesson; 15/sfkgyqdun9p.py
@@ -0,0 +1,13 @@
+v1=float(input())
+try:
+    v2 = v1%2
+    g=(78/v2)
+    print("this is not divisible by two!")
+except ZeroDivisionError:
+    print("this is divisible by two! :)")
+
+    
+    
+
+
+
