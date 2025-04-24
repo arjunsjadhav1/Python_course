f96f3ca3e8ab05fe0a36b6310e22abfebfc1430c
Arjun <arjun.s.jadhav1@gmail.com>
2025-04-24 20:13:41 +0800

    first_publish

--- lesson; 15/chalendroo.py
+++ lesson; 15/chalendroo.py
@@ -1,7 +1,17 @@
 a=int(input("enter lower range :)"))
 b=int(input("enter upper range :)"))
 l2=[]
+l3=[]
+l4=[]
+v3=0
 v1=0
 for v2 in range (a,(b+1)):
    l2.append(v2**2)
-print(l2)
+   if (l2[v3])%2 == 0:
+      l3.append(l2[v3])
+   else:
+      l4.append(l2[v3])
+   v3 = (v3+1)
+print("even:",l3)
+print("odd: ",l4)
+
