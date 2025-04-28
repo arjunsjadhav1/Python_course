b846e3924a554de1429a95557b172d681f054cd5
Arjun <arjun.s.jadhav1@gmail.com>
2025-04-28 19:45:15 +0800

    first_publish

---
+++ lesson; 15/fiwldIA.py
@@ -0,0 +1,14 @@
+import calendar
+print("january")
+print("febuary")
+print("March")
+print("april")
+print("may")
+print("june")
+print("july")
+print("august")
+print("septemeber")
+print("october")
+print("november")
+print("december")
+# how does this have anything to do with the calendar library? we dont need to use any funtions of the calender library in this script.
